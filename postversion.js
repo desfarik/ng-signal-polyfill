41d6dc8c5613f6e624cf1b48c54e5296325f73f5
Dima Tarasyuk <d.tarasyuk@tinkoff.ru>
2024-11-11 14:33:03 +0300

    Sync lib version

--- postversion.js
+++ postversion.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 const path = require('path');
 
 const rootPackagePath = path.join(__dirname, 'package.json');
-const libPackagePath = path.join(__dirname, 'projects', 'lib', 'packages');
+const libPackagePath = path.join(__dirname, 'projects', 'lib', 'package.json');
 
 const rootPackageJson = require(rootPackagePath);
 const childPackageJson = require(libPackagePath);
 
 childPackageJson.version = rootPackageJson.version;
-fs.writeFileSync(childPackageJson, JSON.stringify(childPackageJson, null, 2));
+fs.writeFileSync(libPackagePath, JSON.stringify(childPackageJson, null, 2));
 
